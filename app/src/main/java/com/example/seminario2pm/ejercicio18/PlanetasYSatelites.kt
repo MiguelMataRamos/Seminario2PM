1f4b7dfc8753b27c9626ccd16230940e20647937
MiguelMataRamos <131800578+MiguelMataRamos@users.noreply.github.com>
2023-11-13 20:56:43 +0100

    ej18 haciendose

---
+++ app/src/main/java/com/example/seminario2pm/ejercicio18/PlanetasYSatelites.kt
@@ -0,0 +1,26 @@
+//Define una jerarquía de clases que permita almacenar datos sobre los
+//planetas y satélites que forman parte del sistema solar (junto con el sol).
+//Algunos atributos que puede ser interesante recoger son: la masa del
+//cuerpo, su diámetro medio, el período de rotación sobre el propio eje,
+//período de traslación alrededor del cuerpo que orbitan, distancia media a
+//ese cuerpo, excentricidad de la órbita, etc.
+//Define un método que, dado un objeto del sistema solar (planeta o satélite),
+//imprima toda la información de que se dispone sobre el mismo.
+package com.example.seminario2pm.ejercicio18
+
+open class CuerpoCeleste(
+    val nombre: String, val masa: Double,
+    val diametro: Double,
+    val periodoRotacion: Double,
+    val distanciaMediaAlCuerpo: Double
+) {
+    override fun toString(): String {
+        return "Información sobre $nombre:\n" +
+                "Masa: $masa kg\n" +
+                "Diámetro medio: $diametro km\n" +
+                "Período de rotación: $periodoRotacion horas\n" +
+                "Distancia media al cuerpo que orbita: $distanciaMediaAlCuerpo km\n"
+    }
+}
+
+
