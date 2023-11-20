d811b8bf900c4a8470879e9909a22e2bc55f8688
EAG <mataramosmiguel@gmail.com>
2023-11-20 20:11:57 +0100

    ej 18 hecho perfe

--- app/src/main/java/com/example/seminario2pm/ejercicio18/PlanetasYSatelites.kt
+++ app/src/main/java/com/example/seminario2pm/ejercicio18/PlanetasYSatelites.kt
@@ -9,21 +9,61 @@
 package com.example.seminario2pm.ejercicio18
 
 open class CuerpoCeleste(
-    val nombre: String, val masa: Double,
+    val nombre: String,
+    val masa: Double,
     val diametro: Double,
     val periodoRotacion: Double,
-    val distanciaMediaAlCuerpo: Double
+    val distanciaMedia: Double
 ) {
     override fun toString(): String {
         return "Información sobre $nombre:\n" +
                 "Masa: $masa kg\n" +
                 "Diámetro medio: $diametro km\n" +
                 "Período de rotación: $periodoRotacion horas\n" +
-                "Distancia media al cuerpo que orbita: $distanciaMediaAlCuerpo km\n"
+                "Distancia media al cuerpo que orbita: $distanciaMedia km\n"
     }
 }
 
-class Planeta(nombre: String,diametro: Double,periodoRotacion: Double,distanciaMediaAlCuerpo: Double)
+class Planeta(
+    nombre: String,
+    masa: Double,
+    diametro: Double,
+    periodoRotacion: Double,
+    distanciaMedia: Double,
+    val excentricidadOrbita: Double,
+    val periodoTraslacion: Double
+) : CuerpoCeleste(nombre, masa, diametro, periodoRotacion, distanciaMedia) {
+    override fun toString(): String {
+        return "---Soy un planeta---\n" + super.toString() +
+                "Excentridad de la orbita: $excentricidadOrbita\n " +
+                "Periodo translacion: $periodoTraslacion"
+    }
+}
 
 
+class Satelite(
+    nombre: String,
+    masa: Double,
+    diametro: Double,
+    periodoRotacion: Double,
+    distanciaMedia: Double,
+    val periodoTraslacion: Double
+) : CuerpoCeleste(nombre, masa, diametro, periodoRotacion, distanciaMedia) {
+    override fun toString(): String {
+        return "---Soy un satelite---\n" + super.toString() +
+                "Periodo translacion: $periodoTraslacion"
 
+    }
+}
+
+fun info(p: CuerpoCeleste) {
+    println(p.toString())
+}
+
+fun main() {
+    val tierra = Planeta("Tierra", 5.97e24, 12742.0, 24.0, 365.25, 149.6e6, 0.0167)
+    val luna = Satelite("Luna", 7.35e22, 3475.0, 27.3, 27.3, 384400.0)
+    info(tierra)
+    info(luna)
+
+}
